39d876938ef1e6b57f24b33a5039e9fe01d616aa
Gonza-DelPriore <gonzadelprio@gmail.com>
2026-05-17 17:46:44 -0300

    Función execute en db.py, endpoint GET de Grupos en services/grupos.py.

--- Backend/curso/db.py
+++ Backend/curso/db.py
@@ -8,3 +8,15 @@ def get_connection():
         password="",
         database="curso_universitario"
     )
+
+def execute(query):
+    connection = get_connection()
+    cursor = connection.cursor(dictionary=True)
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+    finally:
+        cursor.close()
+        connection.close
+    return results
+
