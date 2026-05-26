54536526114e0440820bff209f49661ac4ed1ce1
yapu_115 <yapufranco115@gmail.com>
2026-05-26 18:50:51 -0300

    correcciones del login y el registro, protección de rutas

--- Frontend/routes/home.py
+++ Frontend/routes/home.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from utils.auth import login_required
 
 home_bp = Blueprint('home', __name__)
 
 
 @home_bp.route('/')
+@login_required  
 def index():
     return render_template('base.html')
