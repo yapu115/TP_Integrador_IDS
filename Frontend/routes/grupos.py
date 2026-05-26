7a529ca8c7eff92c0842465502e5db97ab10a153
Gonza-DelPriore <gonzadelprio@gmail.com>
2026-05-26 16:10:31 -0300

    Form de seleccion de grupo en grupos.html, blueprint de grupos en grupos.py

---
+++ Frontend/routes/grupos.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+grupos_bp = Blueprint('grupos', __name__)
+
+@app.route("/grupos", methods=["GET", "POST"])
+def grupos():
+    #Guardar grupos en un diccionario y pasarlos al template
+    if request.method == "POST":
+        #Si el metodo es post, se deben pasar los alumnos del grupo seleccionado
+        return render_template("grupos.html")
+    return render_template("grupos.html")
