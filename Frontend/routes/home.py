2ecfd891d51c17af1f1ee1f165bfd4e5c31a7083
Franco Miniaci <franco@fr.localdomain>
2026-05-27 19:31:31 +0000

    Solucion de problema en base.js que causaba que no funcione, modificacion del css de evaluaciones.css y base.css

--- Frontend/routes/home.py
+++ Frontend/routes/home.py
@@ -4,6 +4,6 @@ from utils.auth import login_required
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
-@login_required  
+#@login_required  
 def home():
     return render_template('home.html')
